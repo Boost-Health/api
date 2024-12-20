4a362a41802f6e8e00a4d8525d0e39a082be8e00
Segun Olaiya <vadeshayo@gmail.com>
2024-12-20 14:25:52 +0100

    Completed fixing Logging

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,7 @@
 
 use App\Http\Controllers\Api\Channels\Telegram\SetWebhookController;
 use App\Http\Controllers\Api\Channels\Telegram\WebhookController;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
-use Monolog\Handler\SyslogUdpHandler;
-use Monolog\Logger;
 
 Route::get('/', fn () => response()->json([
     'status' => 'Successful',
@@ -19,24 +16,3 @@ Route::group(['prefix' => '/bot'], function () {
         Route::any('/{token}/webhook', WebhookController::class);
     });
 });
-
-Route::get('/log', function () {
-    Log::error('test:log', ['context' => 'test', 'config' => config('app')]);
-
-    Log::channel('papertrail')->info('LOG:CHANNEL');
-    Log::channel('stdout')->info('Test log for Stdout');
-
-    $log = new Logger('papertrail');
-    $handler = new SyslogUdpHandler('logs5.papertrailapp.com', 49643);
-    $log->pushHandler($handler);
-
-    $log->info('LOG:MANUAL');
-
-    return response()->json([
-        'app' => config('app'),
-        'logging' => config('logging'),
-        'env' => $_ENV,
-    ]);
-});
-
-Route::get('/exception', fn () => throw new \Exception('Testing Sentry'));
