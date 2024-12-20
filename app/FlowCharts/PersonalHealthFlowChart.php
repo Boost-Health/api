884dcfd3fb7e60e54a9902d98a427b27738ab3d6
Segun Olaiya <vadeshayo@gmail.com>
2024-12-20 19:12:02 +0100

    Added Logs

--- app/FlowCharts/PersonalHealthFlowChart.php
+++ app/FlowCharts/PersonalHealthFlowChart.php
@@ -40,6 +40,8 @@ final class PersonalHealthFlowChart extends BaseFlowChart
             }
         }
 
+        Log::info('personal:health:flow', ['response' => $responseText]);
+
         return new FlowChartNextObject('init', [$responseText]);
     }
 
