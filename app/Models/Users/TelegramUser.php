62e7ead6c5f3f4c6f899c79b5c76d24fa4cd8b4b
Segun Olaiya <vadeshayo@gmail.com>
2025-07-04 12:07:31 +0100

    feat: Use markdown for telegram consume

--- app/Models/Users/TelegramUser.php
+++ app/Models/Users/TelegramUser.php
@@ -50,7 +50,7 @@ class TelegramUser extends AbstractUser
         $messageObject = [
             'chat_id' => $this->telegram_chat_id,
             'text' => $message->body,
-            'parse_mode' => 'Markdown',
+            'parse_mode' => 'HTML',
         ];
 
         if (app()->environment('local')) {
