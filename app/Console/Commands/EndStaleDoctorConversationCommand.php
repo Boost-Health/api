6420b29cf1af08e7193ac7370f75f4c09b7a18b6
Segun Olaiya <vadeshayo@gmail.com>
2025-07-06 19:15:14 +0100

    feat: Fix command

--- app/Console/Commands/EndStaleDoctorConversationCommand.php
+++ app/Console/Commands/EndStaleDoctorConversationCommand.php
@@ -23,7 +23,7 @@ class EndStaleDoctorConversationCommand extends Command
             ->get();
 
         foreach ($doctorsWithActiveConversations as $doctorWithActiveConversation) {
-            $this->info('stale:conversation:processing', ['doctor_id' => $doctorWithActiveConversation->id]);
+            $this->info("stale:conversation:processing:{$doctorWithActiveConversation->id}");
 
             $this->check($doctorWithActiveConversation);
         }
@@ -37,8 +37,8 @@ class EndStaleDoctorConversationCommand extends Command
     {
         $conversationId = $doctor->activeConversation->id;
         $lastMessage = $doctor->activeConversation->last_message();
-        $doctorParticipant = $doctor->activeConversation->getParticipants()->reject(fn ($participant) => $participant->user->is($doctor))->first();
-        $userParticipant = $doctor->activeConversation->getParticipants()->filter(fn ($participant) => $participant->user->is($doctor))->first();
+        $doctorParticipant = $doctor->activeConversation->getParticipants()->filter(fn ($participant) => $participant->user->is($doctor))->first();
+        $userParticipant = $doctor->activeConversation->getParticipants()->reject(fn ($participant) => $participant->user->is($doctor))->first();
 
         if ($lastMessage?->created_at->diffInHours(now()) >= self::NUMBER_OF_HOURS_TO_CHECK_FOR_STALE) {
             $messageObject = new MessageObject(
@@ -50,7 +50,7 @@ class EndStaleDoctorConversationCommand extends Command
 
             app(ConversationService::class)->endConversation($messageObject);
 
-            $this->info('stale:conversation:ended', ['conversation_id' => $conversationId]);
+            $this->info('stale:conversation:ended');
         }
     }
 }
